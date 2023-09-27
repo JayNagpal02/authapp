ecb51b0ba20054453c48a8be8f5a4464383c3eb6
JayNagpal02 <jaynagpal02@gmail.com>
2023-09-27 19:43:03 +0530

    app completed

--- app/components/UserInfo.jsx
+++ app/components/UserInfo.jsx
@@ -1,16 +1,26 @@
+"use client";
+
 import React from "react";
+import { signOut, useSession } from "next-auth/react";
 
 const UserInfo = () => {
+    const { data: session } = useSession();
+
     return (
         <div className="grid place-items-center h-screen">
             <div className="shadow-lg p-8 bg-zinc300/10 flex flex-col gap-2 my-6">
                 <div>
-                    Name: <span className="font-bold">Jay</span>
+                    Name:{" "}
+                    <span className="font-bold">{session?.user?.name}</span>
                 </div>
                 <div>
-                    Email: <span className="font-bold">jay@gmail.com</span>
+                    Email:{" "}
+                    <span className="font-bold">{session?.user?.email}</span>
                 </div>
-                <button className="bg-red-500 text-white font-bold px-6 py-2 my-6">
+                <button
+                    onClick={() => signOut()}
+                    className="bg-red-500 text-white font-bold px-6 py-2 my-6"
+                >
                     Log Out
                 </button>
             </div>
